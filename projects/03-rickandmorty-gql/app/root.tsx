8e591a159958ad8ccbb9be04281afd410bb312bf
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-10 21:24:06 -0600

    feat(projects/03-rickandmorty-gql): Add project dependencies and configure tailwind

--- projects/03-rickandmorty-gql/app/root.tsx
+++ projects/03-rickandmorty-gql/app/root.tsx
@@ -1,5 +1,7 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import tailwind from '~/tailwind.css'
+
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -7,27 +9,31 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: tailwind },
+]
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='es'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        
         <Meta />
         <Links />
       </head>
+
       <body>
         <Outlet />
+        
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
