b31c5a0ae68aa6eedee357bd3c1a3b647a5c038b
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-12 13:54:20 -0600

    feat(projects/03-rickandmorty-gql): Add mocking data

--- projects/03-rickandmorty-gql/app/routes/_index.tsx
+++ projects/03-rickandmorty-gql/app/routes/_index.tsx
@@ -1,13 +1,15 @@
 import { json, type LoaderFunction, type MetaFunction } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
-import ListOfCharacters from '~/components/ListOfCharacters'
+import { useLoaderData, useNavigation } from '@remix-run/react'
 
+import response from '~/mock/response.json'
 import { getCharacters } from '~/graphql/queries.server'
 import type { Characters } from '~/types/api'
+import ListOfCharacters from '~/components/ListOfCharacters'
+import SearchBar from '~/components/SearchBar'
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'New Remix App' },
+    { title: 'Rick And Morty' },
     { name: 'description', content: 'Welcome to Remix!' },
   ]
 }
@@ -16,26 +18,39 @@ export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url)
   const searchParams = new URLSearchParams(url.search)
 
-  const name = searchParams.get('query') || ''
+  const query = searchParams.get('query') || ''
   const page = Number(searchParams.get('page')) || 1
 
-  const result = await getCharacters(name, page)
+  // const result = await getCharacters(query, page)
+  const result = response as Characters
 
-  return json({ result })
+  return json({ result, query, page })
+}
+
+interface LoaderData {
+  result: Characters
+  query: string
+  page: number
 }
 
 export default function Index() {
-  const { result } = useLoaderData<{ result: Characters }>()
+  const { result, page, query } = useLoaderData<LoaderData>()
+  const navigation = useNavigation()
 
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1 className='text-xl font-bold text-center'>
-        Welcome to Remix
-      </h1>
+    <main
+      style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}
+      className='w-[95%] md:w-4/5 mx-auto py-4'
+    >
+      <SearchBar query={query} />
 
-      <ListOfCharacters
-        characters={result.results}
-      />
-    </div>
+      {navigation.state === 'loading' ? (
+        <p>Loading...</p>
+      ) : (
+        <ListOfCharacters
+          characters={result.results}
+        />
+      )}
+    </main>
   )
 }
