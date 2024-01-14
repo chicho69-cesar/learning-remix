4a779eb23e72ad0b51d66484a4e8a8f85ff31358
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-13 20:21:57 -0600

    feat(projects/03-remix-expenses): Remove last name for second project and third project

--- projects/03-rickandmorty-gql/app/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const COOKIE_FAVORITE = 'favorite-characters'
--- projects/03-rickandmorty-gql/app/graphql/config.server.ts
+++
@@ -1,6 +0,0 @@
-import { Client, cacheExchange, fetchExchange } from 'urql'
-
-export const client = new Client({
-  url: 'https://rickandmortyapi.com/graphql',
-  exchanges: [cacheExchange, fetchExchange],
-})
