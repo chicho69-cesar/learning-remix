70e431df922ce2f30307255101e8b303f4bc2578
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-23 21:20:12 -0600

    feat(projects/04-notes-app): Add notes server helper

---
+++ projects/04-notes-app/app/data/notes.server.ts
@@ -0,0 +1,13 @@
+import fs from 'node:fs/promises'
+import type { Note } from '~/types/notes'
+
+export async function getStoredNotes() {
+  const rawFileContent = await fs.readFile('notes.json', { encoding: 'utf-8' })
+  const data = JSON.parse(rawFileContent)
+  const storedNotes = data.notes ?? []
+  return storedNotes as Note[]
+}
+
+export function storeNotes(notes: Note[]) {
+  return fs.writeFile('notes.json', JSON.stringify({ notes: notes || [] }))
+}
