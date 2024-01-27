097c95fcbc14bcbeb98a25922450dd97c433ae01
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-26 21:41:32 -0600

    feat(projects/08-remix-chat): Add emitter and chat subscriptions

---
+++ projects/08-remix-chat/app/routes/chat.subscribe.ts
@@ -0,0 +1,20 @@
+import { LoaderFunction } from '@remix-run/node'
+import { eventStream } from 'remix-utils/sse/server'
+import { emitter } from '~/services/emitter.server'
+
+export const loader: LoaderFunction = ({ request }) => {
+  return eventStream(request.signal, function setup(send) {
+    function listener(value: string) {
+      send({
+        event: 'message',
+        data: value
+      })
+    }
+
+    emitter.on('message', listener)
+
+    return function cleanup() {
+      emitter.off('message', listener)
+    }
+  })
+}
